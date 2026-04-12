f42047eddfd1cbb07a5d55c405249d6d59287603
org-mustakim <mail.mustakim@yahoo.com>
2026-04-12 17:45:50 +0600

    added Issue buttons and made responsive

--- scripts/home.js
+++ scripts/home.js
@@ -1,29 +1,2 @@
-const removeActiveBtn = (element) => {
-    const btnContainer = document.querySelectorAll('.clickable-btn')
-
-    const styelsToRemove = ['btn-primary', 'text-white']
-    const styelsToAdd = ['btn-outline', 'text-[#64748bFF]']
-
-    btnContainer.forEach((btn) => {
-        btn.classList.remove(...styelsToRemove)
-        btn.classList.add(...styelsToAdd)
-    })
-}
-
-const activeBtn = (id) => {
-    const btnToGetActive = document.getElementById(id)
-
-    removeActiveBtn(btnToGetActive)
-
-    const styelsToRemove = ['btn-outline', 'text-[#64748bFF]']
-    const ActiveBtnStyle = ['btn-primary', 'text-white']
-
-    btnToGetActive.classList.remove(...styelsToRemove)
-    btnToGetActive.classList.add(...ActiveBtnStyle)
-}
-
-
-
-
 
 
